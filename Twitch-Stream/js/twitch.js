93eda1d7142d607b97cef9afcb99ffc9361c6ecc
PlayerID <dani3l.gao@gmail.com>
2016-07-10 18:02:17 -0400

    Populate HTML with data

--- Twitch-Stream/js/twitch.js
+++ Twitch-Stream/js/twitch.js
@@ -1,20 +1,82 @@
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
-	"storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var channels = ["ESL_SC2", "OgamingSC2", "brunofin", "cretetion", "freecodecamp",
+	"storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"];
 var twitchEndpoint = "https://api.twitch.tv/kraken/streams/";
+var defaultImage = "https://static-cdn.jtvnw.net/ttv-static/404_preview-200x200.jpg"
+var onlineChannels = [];
+var offlineChannels = [];
+// Options are "all", "online", "offline".
+var showCategory = "all";
 
-function getAllChannelData () {
+function getAllChannelData() {
+	onlineChannels = [];
+	offlineChannels = [];
 	for (var i = 0; i < channels.length; i++) {
 		getChannelData(channels[i]);
 	}
 }
 
-function getChannelData (channel) {
+function getChannelData(channel) {
 	$.getJSON(twitchEndpoint + encodeURI(channel) + "?callback=?", function(data) {
-		console.log(data);
+		// Populate channelInfos
+		if (data.error == "Not Found") {
+			offlineChannels.push({
+				"channel": channel,
+				"message": "Channel Not Found",
+				"category": "error"
+			});
+		} else if (data.stream == null) {
+			offlineChannels.push({
+				"channel": channel,
+				"message": "Offline",
+				"category": "offline"
+			});
+		} else {
+			onlineChannels.push({
+				"channel": channel,
+				"game": data.stream.game,
+				"message": data.stream.channel.status,
+				"image": data.stream.channel.logo,
+				"category": "online"
+			});
+		}
+		populateList();
 	});
 }
 
+function populateList() {
+	$('#results').fadeOut(500, function() {
+		var newHtml = "";
+		if (showCategory == "all" || showCategory == "online") {
+			for (var i = 0; i < onlineChannels.length; i++) {
+				newHtml = newHtml.concat(generateHtml(onlineChannels[i]));
+			}
+		}
+		if (showCategory == "all" || showCategory == "offline") {
+			for (var i = 0; i < offlineChannels.length; i++) {
+				console.log("Add Offline");
+				newHtml = newHtml.concat(generateHtml(offlineChannels[i]));
+			}
+		}
+		$('#results').html(newHtml).fadeIn(500);
+    });
+}
 
+function generateHtml(channelInfo) {
+	var html = "";
+	var channelName = channelInfo.name;
+	var channelMessage = channelInfo.message;
+	var channelImage = defaultImage;
+	if (channelInfo.hasOwnProperty("image") && channelInfo.image != "") {
+		channelImage = channelInfo.image;
+	}
+
+	html = '<div class="channel ' + channelInfo.category
+	+ '"><div class="image"><img src="' + channelImage
+	+ '" alt="channel image"></div><div class="channel-name"><p>' + channelName
+	+ '</p></div><div><p>' + channelMessage + '</p></div>'
+
+	return html;
+}
 
 $(document).ready(function() {
 	getAllChannelData();
@@ -29,7 +91,7 @@ $(document).ready(function() {
 		var newChannel = $('#add-channel').val();
 		if (newChannel != '') {
 			channels.push(newChannel);
-			getChannelData(newChannel);
+			getAllChannelData(newChannel);
 		}
 	});
 });
