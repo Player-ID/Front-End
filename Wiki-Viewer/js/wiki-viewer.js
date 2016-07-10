b05d4b4f04916ae6bb3af317254e481033bf5dc7
PlayerID <dani3l.gao@gmail.com>
2016-07-09 21:23:09 -0400

    Change HTML to show response

--- Wiki-Viewer/js/wiki-viewer.js
+++ Wiki-Viewer/js/wiki-viewer.js
@@ -1,9 +1,19 @@
 var remoteUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&list=search&rvprop=content&srsearch=";
 
 function populateResults (result) {
-	console.log(result);
 	var searchResult = result.query.search;
-	console.log(searchResult);
+	var newHtml = "";
+	for (var i = 0; i < searchResult.length; i++) {
+		var title = searchResult[i].title;
+		var snippet = searchResult[i].snippet;
+
+		var itemTemplate = '<div class="content"><h3>' + title
+			+ '</h3><p>' + snippet + '</p></div>';
+		newHtml = newHtml.concat(itemTemplate);
+	}
+	$('#results').fadeOut(500, function() {
+        $("#results").html(newHtml).fadeIn(500);
+    });
 }
 
 function apiCall (query) {
