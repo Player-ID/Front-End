54203a3e0654383797cfeb99c1216957a8169139
PlayerID <dani3l.gao@gmail.com>
2016-07-09 21:03:45 -0400

    Implement API GET request

--- Wiki-Viewer/js/wiki-viewer.js
+++ Wiki-Viewer/js/wiki-viewer.js
@@ -1,3 +1,30 @@
+var remoteUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&list=search&rvprop=content&srsearch=";
+
+function populateResults (result) {
+	console.log(result);
+	var searchResult = result.query.search;
+	console.log(searchResult);
+}
+
+function apiCall (query) {
+	var requestURL = remoteUrl + encodeURI(query) + "&callback=?";
+	console.log(requestURL);
+	$.getJSON(requestURL, function(json){
+		populateResults(json);
+	});
+}
+
 $(document).ready(function() {
-	
+	console.log("Script is running.");
+	$('#search').bind("enterKey",function(e) {
+		var searchString = $('#search').val();
+		if (searchString != '') {
+			apiCall(searchString);
+		}
+	});
+	$('#search').keyup(function(e) {
+	    if(e.keyCode == 13) {
+	        $(this).trigger("enterKey");
+	    }
+	});
 });
